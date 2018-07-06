70e46270b1b9a3f9c0f1828f008bbd88c4a933f3
Katherine Osterried <katherine.osterried@env.ethz.ch>
2018-07-06 09:25:57 +0200

    Add scripts to run testsuite on kesch

---
+++ test/testsuite/submit.kesch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name="extpar"
+#SBATCH --nodes=1
+#SBATCH --output="job.out"
+#SBATCH --time=03:00:00
+#SBATCH --partition=postproc
+
+module load cdo
+./src/testsuite.py --exe=run_extpar_kesch.sh -v 1 -o testsuite.out --mpicmd='srun -u -n' 
