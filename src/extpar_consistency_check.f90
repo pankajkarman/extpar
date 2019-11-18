1ac1c6d2c10a3fc0d0aba5ad154458abd8f76523
Juergen Helmert <juergen.helmert@dwd.de>
2019-11-18 15:01:42 +0000

    Bugfix for lake ocean neighbors

--- src/extpar_consistency_check.f90
+++ src/extpar_consistency_check.f90
@@ -504,7 +504,6 @@ PROGRAM extpar_consistency_check
 
   INTEGER           :: db_ice_counter,db_water_counter
 
-
   REAL (KIND=wp) :: step
 
   !Special Points
@@ -1834,11 +1833,26 @@ END IF
                           ne_ie(nv) = n_index
                        ENDIF
                     ENDDO
+
+          DO n=1,nnb
+           IF ((ne_ie(n)>= 1).AND.(ne_je(n)>=1).AND.(ne_ke(n)>=1)) THEN
+           IF (fr_ocean_lu(ne_ie(n),ne_je(n),ne_ke(n))>0.5) THEN ! if the direct neighbour element is ocean,
+             fr_lake(i,j,k) = 0.0                                ! set this grid element also to ocean.
+             fr_ocean_lu(i,j,k) = 1.0 - fr_land_lu(i,j,k)
+             lake_depth(i,j,k) = flake_depth_undef ! set lake depth to flake_depth_undef (-1 m)
+           ENDIF  
+           ENDIF
+         ENDDO
+
                  ENDIF
               ENDDO
            ENDDO
         ENDDO
      ENDDO
+
+
+
+
 
      ! manual correction for lake depth of Lake Constance
      ! the raw database of the lake depth data appears not to be correct
