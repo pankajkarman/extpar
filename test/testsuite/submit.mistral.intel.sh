396c0f62a84b4ddc3c5b54433dda0021b5731750
Jonas_Jucker <39263956+jonasjucker@users.noreply.github.com>
2020-03-02 16:26:26 +0100

    fix for intel runscripts (#139)

--- test/testsuite/submit.mistral.intel.sh
+++ test/testsuite/submit.mistral.intel.sh
@@ -7,6 +7,6 @@
 #SBATCH --account=mh0287
 
 ./src/testsuite.py --exe=run_extpar_mch.sh -v 1 -o testsuite.out --testlist=testlist_cosmo_intel.xml --mpicmd='srun -u -n' 
-./src/testsuite.py -a --exe=run_extpar_mistral_dwd.sh -v 1 -o testsuite.out --testlist=testlist_cosmo_dwd_intel.xml --mpicmd='srun -u -n'  
+./src/testsuite.py -a --exe=run_extpar_dwd.sh -v 1 -o testsuite.out --testlist=testlist_cosmo_dwd_intel.xml --mpicmd='srun -u -n'  
 module load python
 ./src/testsuite.py -a --exe=run_extpar_mistral_mpim.sh -v 1 -o testsuite.out --testlist=testlist_icon_intel.xml --mpicmd='srun -u -n'  
