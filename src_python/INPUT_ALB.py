bc165ede1930bed5360de127dd7b62455671cd50
Jonas Jucker <joni.jucker@yahoo.de>
2020-03-28 10:30:08 +0100

    wrapper for cdo

--- src_python/INPUT_ALB.py
+++ src_python/INPUT_ALB.py
@@ -1,11 +1,12 @@
 # namelist for extpar_alb_to_buffer.py
 # is identical with the corresponding fortran namelist
 
-raw_data_alb="testfile"
+raw_data_alb="month_alb_new.nc"
 raw_data_path=""
-raw_data_aluvd="testfile"
-raw_data_alnid="testfile"
+raw_data_aluvd="month_aluvd_new.nc"
+raw_data_alnid="month_alnid_new.nc"
 buffer_alb=""
+grid_spec="grid_spec.txt"
 
 
 
--- src_python/alb_to_buffer.py
+++ src_python/alb_to_buffer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3 
 import logging
 import os
 import sys
@@ -5,6 +6,7 @@ import subprocess
 
 import INPUT_ALB as nl
 import io_utilities as io_utils
+import shell_wrapper as sw
 
 
 # initialize logger
@@ -27,19 +29,22 @@ io_utils.check_existence(nl.raw_data_path, nl.raw_data_alb)
 io_utils.check_existence(nl.raw_data_path, nl.raw_data_aluvd)
 io_utils.check_existence(nl.raw_data_path, nl.raw_data_alnid)
 
-omp_num_threads=8
+omp=8
 
 if os.path.exists("weights.nc"):
     os.remove("weights.nc")
 
+# calculate weights
+sw.launch_shell('cdo','-f', 'nc4','-P', omp,f'gendis,{nl.grid_spec}',nl.raw_data_alb, 'weights.nc')
 
-file="test.nc"
-subprocess.check_output(["ls", "-l"])
-try:
-    shell_message=subprocess.check_output(["cdo", "info",file], stderr=subprocess.STDOUT)
-except subprocess.CalledProcessError:
-    logging.error('Shell error', exc_info=True)
-    sys.exit(1)
+# regrid 1
+sw.launch_shell('cdo','-f', 'nc4','-P', omp,f'setrtoc,-1000000,0.02,0.02', \
+                f'-remap,{nl.grid_spec},weights.nc',nl.raw_data_alb, 'alb-dis.nc')
+# regrid 2
+sw.launch_shell('cdo','-f', 'nc4','-P', omp,f'setrtoc,-1000000,0.02,0.02', \
+                f'-remap,{nl.grid_spec},weights.nc',nl.raw_data_alnid, 'alnid-dis.nc')
+# regrid 3
+sw.launch_shell('cdo','-f', 'nc4','-P', omp,f'setrtoc,-1000000,0.02,0.02', \
+                f'-remap,{nl.grid_spec},weights.nc',nl.raw_data_aluvd, 'aluvd-dis.nc')
 
-logging.info(shell_message)
 
