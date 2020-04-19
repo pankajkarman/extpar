2321d1c89811d4bdc1cf9af20b210379608a1f6e
Jonas Jucker <joni.jucker@yahoo.de>
2020-04-19 17:34:55 +0200

    update tsa runscripts

--- test/testsuite/submit.tsa.sh
+++ test/testsuite/submit.tsa.sh
@@ -1,1 +1,15 @@
-submit.kesch.sh
+#!/bin/bash
+#SBATCH --job-name="extpar"
+#SBATCH --nodes=1
+#SBATCH --output="job.out"
+#SBATCH --time=01:00:00
+#SBATCH --partition=postproc
+
+source /oprusers/osm/.opr_setup_dir
+export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
+source /users/juckerj/venv_jenkins/bin/activate
+module load cdo
+module list
+export HDF5_DISABLE_VERSION_CHECK=1
+./src/testsuite.py --exe=run_extpar_cosmo.sh -v 1 -o testsuite.out --testlist=testlist_cosmo.xml --mpicmd='srun -u -n'  
+
