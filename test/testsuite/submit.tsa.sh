204d62f0519e8baa74da66f3d09176a4a31cced2
Jonas Jucker <joni.jucker@yahoo.de>
2020-04-27 14:54:23 +0200

    debug prints

--- test/testsuite/submit.tsa.sh
+++ test/testsuite/submit.tsa.sh
@@ -7,12 +7,10 @@
 
 source /oprusers/osm/.opr_setup_dir
 export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
-module load gnu_PE/17.02 
-module load PrgEnv-gnu 
-module load netcdf/4.2.1.1-gnu-5.4.0
-source /users/juckerj/venv_jenkins/bin/activate
+source ../../modules.env
 module load cdo
 export HDF5_DISABLE_VERSION_CHECK=1
 printenv > env.log
+module list > list.log
 
 ./src/testsuite.py --exe=run_extpar_cosmo.sh -v 1 -o testsuite.out --testlist=testlist_cosmo.xml --mpicmd='srun -u -n'
