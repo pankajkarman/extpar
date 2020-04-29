723616429d76d0ed7a1958357af2372f7b150068
Jonas_Jucker <joni.jucker@yahoo.de>
2020-04-29 10:57:31 +0200

    add GCC references

--- test/testsuite/data/get_data.sh
+++ test/testsuite/data/get_data.sh
@@ -14,31 +14,31 @@ fi
 # mch
 test -d mch || exit 1
 cd mch/c7_globe
-wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_mch_c7_PR159.nc'
+wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_mch_c7_PR164.nc'
 cd -
 
 test -d mch || exit 1
 cd mch/c1_aster
-wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_mch_c1_PR159.nc'
+wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_mch_c1_PR164.nc'
 cd -
 
 # clm
 test -d clm || exit 1
 cd clm/12km_globe
-wget --quiet 'ftp://iacftp.ethz.ch/pub_read/silvertk/external_parameter_12km_globe_PR143.nc'
+wget --quiet 'ftp://iacftp.ethz.ch/pub_read/silvertk/external_parameter_12km_globe_PR164.nc'
 cd -
 
 # dwd
 test -d dwd || exit 1
 cd dwd/cde2_globe
-wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_cde2_dwd_PR159.nc'
+wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_cde2_dwd_PR164.nc'
 cd -
 
 cd dwd/icon_d2
 wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/icon_grid_DOM01.nc'
 wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/ei_2t_an1986-2015_domain2_DOM01_BUFFER.nc'
 wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/ei_an1986-2015_domain2_DOM01_BUFFER.nc'
-wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_icon_domain2_DOM01_tiles.nc'
+wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_icon_domain2_DOM01_tiles_PR164.nc'
 cd -
 
 # mpim
@@ -47,7 +47,7 @@ cd mpim/icon_r2b4
 wget --quiet 'http://icon-downloads.mpimet.mpg.de/grids/public/mpim/0013/icon_grid_0013_R02B04_G.nc'
 wget --quiet 'ftp://iacftp.ethz.ch/pub_read/silvertk/ei_t2m_an1986-2015_0013_R02B04_G_BUFFER.nc'
 wget --quiet 'ftp://iacftp.ethz.ch/pub_read/silvertk/ei_sst_an1986-2015_0013_R02B04_G_BUFFER.nc'
-wget --quiet 'ftp://iacftp.ethz.ch/pub_read/silvertk/external_parameter_icon_mpim_PR125.nc'
+wget --quiet 'ftp://iacftp.ethz.ch/pub_read/silvertk/external_parameter_icon_mpim_PR164.nc'
 cd -
 
 # intel references
