3e63b8053f5f6512f3f5a150f08aab91e0072170
Jonas_Jucker <joni.jucker@yahoo.de>
2020-04-30 13:15:15 +0200

    update GCC references

--- test/testsuite/data/get_data.sh
+++ test/testsuite/data/get_data.sh
@@ -25,7 +25,7 @@ cd -
 # clm
 test -d clm || exit 1
 cd clm/12km_globe
-wget --quiet 'ftp://iacftp.ethz.ch/pub_read/silvertk/external_parameter_12km_globe_PR143.nc'
+wget --quiet 'ftp://iacftp.ethz.ch/pub_read/juckerj/external_parameter_12km_globe_PR170.nc'
 cd -
 
 # dwd
