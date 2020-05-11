bc349e304fac388e96fed4933fb81316d6914d16
Jonas Jucker <joni.jucker@yahoo.de>
2020-05-11 13:12:16 +0200

    load netcd-python for jenkins

--- test/testsuite/submit.daint.sh
+++ test/testsuite/submit.daint.sh
@@ -7,6 +7,7 @@ module load daint-gpu
 module load CDO
 source ../../modules.env
 module load cray-python
+module load netcdf-python
 echo $PYTHONPATH
 ./src/testsuite.py --exe=run_extpar_cosmo.sh -v 3 -o testsuite.out --testlist=testlist_cosmo.xml --mpicmd='srun -u -n'  
 ./src/testsuite.py -a --exe=run_extpar_icon.sh -v 3 -o testsuite.out --testlist=testlist_icon.xml --mpicmd='srun -u -n'  
