223e0ed58c8acdcd9043e15215bec3989ca1abeb
Jonas Jucker <joni.jucker@yahoo.de>
2020-05-25 11:23:22 +0200

    use g110 env for daint

--- test/testsuite/submit.daint.sh
+++ test/testsuite/submit.daint.sh
@@ -2,11 +2,10 @@
 #SBATCH --constraint=gpu
 #SBATCH --output="job.out"
 #SBATCH --account=g110
-#SBATCH --time=01:00:00
+#SBATCH --time=01:30:00
 module load daint-gpu
 module load CDO
 source ../../modules.env
-module load cray-python
-module load netcdf-python
+source /project/g110/extpar_envs/venv_jenkins_daint/bin/activate
 
 ./src/testsuite.py --exe=run_extpar_cosmo.sh -v 1 -o testsuite.out --testlist=testlist_cosmo.xml --mpicmd='srun -u -n'  
